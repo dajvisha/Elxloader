635092d117fd320f30a9ed8153408dad3e415fe6
diegoajv <diegojosevilla@outlook.com>
2019-06-26 22:13:44 -0500

    Feat[graphql-queries]: Add list query to schema

--- lib/graphql/api_schema.ex
+++ lib/graphql/api_schema.ex
@@ -1,9 +1,14 @@
 defmodule GraphQL.ApiSchema do
   use Absinthe.Schema
 
+  alias GraphQL.ApiResolvers
+
   import_types(GraphQL.ApiTypes)
 
   query do
-    # Some queries here
+    field :list, :list do
+      arg(:id, non_null(:id))
+      resolve(&ApiResolvers.find_list/3)
+    end
   end
 end
