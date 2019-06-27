982af23ce90b1f57dfe990f39b426b4033f092ac
diegoajv <diegojosevilla@outlook.com>
2019-06-27 03:29:16 -0500

    Feat[graphql-types]: Define types

--- lib/graphql/api_types.ex
+++ lib/graphql/api_types.ex
@@ -1,11 +1,19 @@
 defmodule GraphQL.ApiTypes do
   use Absinthe.Schema.Notation
   use Absinthe.Ecto, repo: Niffler.Repo
-  
+
+  alias GraphQL.ApiResolvers
+
   object :list do
+    # Attributes
     field :id, :id
     field :name, :string
     field :properties, :list_properties
+
+    # Relationships
+    field :items, list_of(:item) do
+      resolve(&ApiResolvers.find_list_items/3)
+    end
   end
 
   object :price do
@@ -17,6 +25,11 @@ defmodule GraphQL.ApiTypes do
   object :item do
     field :product_code, :string
     field :name, :string
+
+    # Relationships
+    field :price, :price do
+      resolve(&ApiResolvers.find_item_price/3)
+    end
   end
 
   object :list_properties do
