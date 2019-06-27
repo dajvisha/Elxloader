861c4f462ae63b6f81d425ad9fcd470140764c0d
diegoajv <diegojosevilla@outlook.com>
2019-06-26 23:32:57 -0500

    Feat[dataloader]: Set up Dataloader

--- lib/graphql/api_schema.ex
+++ lib/graphql/api_schema.ex
@@ -1,9 +1,27 @@
 defmodule GraphQL.ApiSchema do
   use Absinthe.Schema
 
+  alias Cashier
   alias GraphQL.ApiResolvers
 
   import_types(GraphQL.ApiTypes)
+
+  # Dataloader Configuration
+
+  def plugins do
+    [Absinthe.Middleware.Dataloader | Absinthe.Plugin.defaults]
+  end
+
+  def dataloader() do
+    Dataloader.new
+    |> Dataloader.add_source(Cashier, Cashier.data())
+  end
+
+  def context(ctx) do
+    Map.put(ctx, :loader, dataloader())
+  end
+
+  # Schema Configuration
 
   query do
     field :list, :list do
