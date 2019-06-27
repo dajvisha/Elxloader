e73b4da9b95119ed131bb8a40fdd327a446ad085
diegoajv <diegojosevilla@outlook.com>
2019-06-26 22:19:23 -0500

    Chore: Add dataloader dep

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Elxloader.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:absinthe, "~> 1.4"},
       {:absinthe_ecto, "~> 0.1.3"},
-      {:absinthe_plug, "~> 1.4"}
+      {:absinthe_plug, "~> 1.4"},
+      {:dataloader, "~> 1.0.0"}
     ]
   end
 
